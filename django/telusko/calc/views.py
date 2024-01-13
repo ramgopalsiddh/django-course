384cd0bb026553a38f2292ffd92f5b1a6253abc4
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-01-13 21:23:24 +0530

    Add 2 number(input number GET by form ) and print result in new page

--- django/telusko/calc/views.py
+++ django/telusko/calc/views.py
@@ -4,4 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello World')
+    return render(request, 'home.html', {'name':'Ram Gopal'})
+
+def add(request):
+
+    val1 = int(request.GET["Num 1"])
+    val2 = int(request.GET["Num 2"])
+    res = val1 + val2
+
+    return render(request, 'result.html', {'result':res})
