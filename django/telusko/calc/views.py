41922fc34eeedb1508a5cab9a24270cc43b6839d
Ram Gopal Siddh <ramgopalsiddh.000@gmail.com>
2024-01-13 22:48:32 +0530

    Use POST method and hide input data from url

--- django/telusko/calc/views.py
+++ django/telusko/calc/views.py
@@ -8,8 +8,8 @@ def home(request):
 
 def add(request):
 
-    val1 = int(request.GET["Num 1"])
-    val2 = int(request.GET["Num 2"])
+    val1 = int(request.POST["Num 1"])
+    val2 = int(request.POST["Num 2"])
     res = val1 + val2
 
     return render(request, 'result.html', {'result':res})
